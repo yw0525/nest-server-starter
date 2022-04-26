1100c27779c134b1fdcf44157264b7f7c4e83c44
heora <yangzhiqiang@winwin-inc.com>
2022-04-26 18:33:16 +0800

    perf: update posts.service

--- src/posts/posts.service.ts
+++ src/posts/posts.service.ts
@@ -1,4 +1,10 @@
 import { Injectable } from '@nestjs/common';
+import { PostsEntity } from './entity/posts.entity';
+
+export interface PostsResult {
+  list: PostsEntity[];
+  count: number;
+}
 
 @Injectable()
 export class PostsService {}

56261a3f708eca68810bfffa78ce6c8c9d4c87be
heora <yangzhiqiang@winwin-inc.com>
2022-04-26 18:25:22 +0800

    feat: add posts module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,38 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { PostsModule } from './posts/posts.module';
+
+import env from './config/env';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+      envFilePath: [env.path],
+    }),
+    TypeOrmModule.forRootAsync({
+      imports: [ConfigModule],
+      inject: [ConfigService],
+      useFactory: async (service: ConfigService) => {
+        return {
+          type: 'mysql',
+          entities: [],
+          host: service.get('DB_HOST'),
+          port: service.get<number>('DB_PORT'),
+          username: service.get('DB_USER'),
+          password: service.get('DB_PASSWORD'),
+          database: service.get('DB_DATABASE'),
+          timezone: '+08:00',
+          synchronize: true,
+        };
+      },
+    }),
+    PostsModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })

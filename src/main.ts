5877d4cfca0360e51f9e788e1f6d1ea926e8a19e
heora <yangzhiqiang@winwin-inc.com>
2022-04-27 09:55:11 +0800

    perf: use interceptor and filter

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,14 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { HttpExceptionFilter } from './core/filter/http-exception.filter';
+import { TransformInterceptor } from './core/interceptor/transform.interceptor';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  app.useGlobalFilters(new HttpExceptionFilter());
+  app.useGlobalInterceptors(new TransformInterceptor());
+
   await app.listen(3000);
 }
 bootstrap();
